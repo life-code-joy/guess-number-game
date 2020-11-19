ff318376ee8b0f0930469e25ef303ad248b2e2bf
darren <darren__c@hotmail.com>
2020-11-19 13:43:23 +0900

    game logic complete-next add css to win screen

--- script.js
+++ script.js
@@ -10,15 +10,46 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value); 
 */
 
+const secretNumber = Math.trunc((Math.random() * 20));
+let score = 20; 
 
 document.querySelector('.check').addEventListener('click', function(){
   const guess = Number(document.querySelector('.guess').value);
   console.log(typeof guess, guess);
 
+
+  //WHEN THERE IS NO INPUT
   if(!guess) {
     document.querySelector('.message').textContent = 'No Number!!';
-  }
+
+    //WHEN PLAYER WINS
+  } else if(guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Yay correct number!!';
+    //WEN GUESS IS TOO HIGH
+  } else if(guess > secretNumber) {
+    if(score > 1){
+      document.querySelector('.message').textContent = 'Too high'
+    score --;
+    document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = "You lost the game. 🖕 Try again!"
+      document.querySelector('.score').textContent = 0;
+    }
+    
+    //IF GUESS IS TOO LOW
+  } else if (guess < secretNumber) {
+    if(score > 1){
+      document.querySelector('.message').textContent = 'Too low'
+    score --;
+    document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = "You lost the game. 🖕 Try again!"
+      document.querySelector('.score').textContent = 0;
+    }
+  } 
 });
+   
 
 
 
+console.log(secretNumber);
